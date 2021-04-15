c08844ceb4a7185a6acbebd3a6dc12926e9d4670
Dmitriy Gerasimov <naeper@demlabs.net>
2021-04-15 13:22:50 +0700

    Forget to add 000000000 to the supply level

--- contracts/core/assets/kel/KEL.sol
+++ contracts/core/assets/kel/KEL.sol
@@ -6,6 +6,6 @@ import "./../../../libs/token/ERC20/ERC20Detailed.sol";
 
 contract KEL is Context, ERC20, ERC20Detailed {
     constructor () public ERC20Detailed("KelVPN token", "KEL", 9) {
-        _mint(_msgSender(), 100000000 );
+        _mint(_msgSender(), 100000000000000000 );
     }
 }
